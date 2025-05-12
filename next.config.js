55d6c50e5ddb90fadbfbe97b379228a113bfc43e
Steve McGhee <steve@mcgheemail.com>
2025-05-12 13:04:17 -0700

    fix prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/centralcoastswim.com' : '',
   images: {
     unoptimized: true,
   },
