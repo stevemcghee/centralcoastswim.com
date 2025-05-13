845e81748503110f5363536fe3eb8e144c1532ea
Steve McGhee <steve@mcgheemail.com>
2025-05-13 09:29:21 -0700

    fix: update static export configuration for Next.js 14

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    unoptimized: false,
+    unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
